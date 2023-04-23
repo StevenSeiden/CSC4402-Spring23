1cca5fd115f413cab2ae60f5c9eab55e07e73c77
lmoreau21 <61840342+lmoreau21@users.noreply.github.com>
2023-04-23 17:49:41 -0500

    test

--- index.php
+++ index.php
@@ -864,26 +864,7 @@
                       </div>
                     </div>
                   </div>
-                  <div class="col-lg-6">
-                    <div class="d-flex align-items-center">
-                      <img
-                        class="flex-shrink-0 img-fluid rounded"
-                        src="img/menu-8.jpg"
-                        alt=""
-                        style="width: 80px"
-                      />
-                      <div class="w-100 d-flex flex-column text-start ps-4">
-                        <h5
-                          class="d-flex justify-content-between border-bottom pb-2"
-                        >
-                          <span>Chicken Burger</span>
-                          <span class="text-primary">$115</span>
-                        </h5>
-                        <small class="fst-italic"
-                          >Ipsum ipsum clita erat amet dolor justo diam</small
-                        >
-                      </div>
-                    </div>
+                  
                   </div>
                 </div>
               </div>
