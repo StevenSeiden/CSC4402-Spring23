4dc47517416be724534d05baeff75a06a8f0e712
Steven Seiden <53193445+StevenSeiden@users.noreply.github.com>
2023-04-29 16:46:05 -0500

    Fixed footer format in dark mode

--- cart.php
+++ cart.php
@@ -196,11 +196,13 @@
         }
       </script>
  <!-- Footer Start -->
+
+ <br>
  <div
-        class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn"
+        class="container-fluid bg-dark text-light footer wow fadeIn"
         data-wow-delay="0.1s"
       >
-        <div class="container py-5">
+        <div class="container bg-dark  py-5">
           <div class="row g-5">
             <div class="col-lg-3 col-md-6">
               <h4
